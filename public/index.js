314d8db79a81b187e4cb0308c45e027ea6c7c6a3
zoheb598 <joheb133@gmail.com>
2022-11-15 20:01:17 +0000

    Enemy: created enemy class
    Enemies spawn around a circular perimeter with different distance around said perimeter

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
-const canvas = document.querySelector('canvas')!;
-const ctx = canvas.getContext('2d')!;
-
+"use strict";
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 window.addEventListener('resize', () => {
@@ -9,80 +9,63 @@ window.addEventListener('resize', () => {
     player.resize();
     enemyList.forEach(element => {
         element.resize();
-    })
-})
-
-//distance function
-function distance(x1: number, y1: number, x2: number, y2: number) {
+    });
+});
+function distance(x1, y1, x2, y2) {
     let xDistance = x2 - x1;
     let yDistance = y2 - y1;
-
     return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 }
-
-//random function
-function getRndInteger(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 window.addEventListener('click', () => {
     enemyList.forEach(element => {
         element.x = getRndInteger(innerWidth / 2, innerWidth);
-        element.y = getRndInteger(innerHeight / 2, innerHeight)
-    })
+        element.y = getRndInteger(innerHeight / 2, innerHeight);
+    });
 });
-
-//enemy
 class Enemy {
-    x: number;
-    y: number;
-    radius: number = innerWidth / 100;
-    constructor(x: number, y: number) {
+    constructor(x, y) {
+        this.radius = innerWidth / 100;
         this.x = x;
-        this.y = y
+        this.y = y;
     }
-
     draw() {
-        ctx.beginPath()
+        ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        ctx.fillStyle = '#FFFFFF'
+        ctx.fillStyle = '#FFFFFF';
         ctx.fill();
-    };
-
+    }
+    ;
     resize() {
         this.radius = innerWidth / 50;
     }
-
 }
-
-//player
 class Player {
-    x: number = innerWidth / 2;
-    y: number = innerHeight / 2;
-    radius: number = innerWidth / 4;
-    health: number = 500;
-
+    constructor() {
+        this.x = innerWidth / 2;
+        this.y = innerHeight / 2;
+        this.radius = innerWidth / 4;
+        this.health = 500;
+    }
     draw() {
-        ctx.beginPath()
+        ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        ctx.fillStyle = '#9BD8AA'
+        ctx.fillStyle = '#9BD8AA';
         ctx.fill();
     }
-
     resize() {
         this.x = innerWidth / 2;
         this.y = innerHeight / 2;
         this.radius = innerWidth / 50;
     }
 }
-
 const player = new Player();
-
-let enemyList: Enemy[] = []; 
-
-function generateParticles(length: number) {
+let enemyList = [];
+function generateParticles(length) {
     const radius = 400;
-    for(let i = 0; i < length; i++){
+    for (let i = 0; i < length; i++) {
         const radian = (Math.PI * 2) / length;
         const x = (innerWidth / 2) + (Math.cos(radian * i) * getRndInteger(radius, radius + 400));
         const y = (innerHeight / 2) + (Math.sin(radian * i) * getRndInteger(radius, radius + 400));
@@ -90,12 +73,8 @@ function generateParticles(length: number) {
         enemyList.push(enemy);
     }
 }
-
-generateParticles(50)
-
-
-console.log(enemyList)
-
+generateParticles(50);
+console.log(enemyList);
 function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
@@ -105,6 +84,4 @@ function animate() {
     });
 }
 animate();
-
-//Distance formula = (x1, y1, x2, y2) - (radius1, radius2) < 0
-//
+//# sourceMappingURL=index.js.map
