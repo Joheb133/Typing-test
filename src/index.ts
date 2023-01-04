1f35f27e63c7c336d950f7c8efb63d4de408c35a
zoheb598 <joheb133@gmail.com>
2023-01-04 18:27:07 +0000

    Added loading screen

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,39 @@ import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
 import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib';
 import Player from './assets/player';
 import EnemyHandler from './assets/EnemyHandler';
-import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper';
-import { SelectiveBloomEffect, EffectComposer, EffectPass, RenderPass, SMAAEffect, SMAAPreset } from "postprocessing";
+import { SelectiveBloomEffect, EffectComposer, EffectPass, RenderPass, SMAAEffect } from "postprocessing";
+import { gsap } from 'gsap'
 
 //setup
-let width: number = window.innerWidth;
-let height: number = window.innerHeight;
 const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const loadingEl = document.querySelector('.loading-screen') as HTMLDivElement;
+const progressEl = document.querySelector('.progress') as HTMLDivElement; //loading bar
+let progress = 0;
 
-const scene = new THREE.Scene(); //create scene
+function updateProgress(amount: number) {
+    progress += amount;
+    gsap.to(progressEl, {
+        width: `+=${amount}%`,
+        duration: 0.5,
+        onComplete: ()=>{
+            if (progress >= 100) {
+                const top = loadingEl.offsetHeight;
+                gsap.to(loadingEl, {
+                    delay: 0.5,
+                    y: -top,
+                    opacity: 0,
+                    duration: 1,
+                    onComplete: () => {
+                        loadingEl.style.display = "none";
+                    }
+                })
+                canvas.style.display = "block";
+            }
+        }
+    })
+}
+//create scene
+const scene = new THREE.Scene();
 
 //renderer
 const renderer = new THREE.WebGLRenderer({
@@ -20,23 +44,23 @@ const renderer = new THREE.WebGLRenderer({
     stencil: false,
     canvas: canvas
 });
-renderer.setSize(width, height)
+renderer.setSize(innerWidth, innerHeight)
 
 //css2drenderer
 const cssRenderer = new CSS2DRenderer();
-cssRenderer.setSize(width, height);
+cssRenderer.setSize(innerWidth, innerHeight);
 cssRenderer.domElement.style.position = 'absolute';
 cssRenderer.domElement.style.top = '0';
 cssRenderer.domElement.className = 'text-renderer'
 document.body.appendChild(cssRenderer.domElement);
 
 //camera
-const camera = new THREE.PerspectiveCamera(40, width / height, 0.1, 500);
+const camera = new THREE.PerspectiveCamera(40, innerWidth / innerHeight, 0.1, 500);
 camera.position.set(-40, 40, 40) //default was -40, 40, 40 //testing is 0, 40, 40
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 //scene lighting
-const spotLight = new THREE.SpotLight(0xffffff, 1, 30, Math.PI/3, 0, 1.25);
+const spotLight = new THREE.SpotLight(0xffffff, 1, 30, Math.PI / 3, 0, 1.25);
 spotLight.position.set(5, 15, 0);
 scene.add(spotLight);
 
@@ -70,10 +94,10 @@ function createPlane() {
     const roughnessMap = textureLoader.load('/textures/terrain-roughness.jpg');
 
     function useEffect() {
-        [normalMap, roughnessMap].forEach((t) =>{
+        [normalMap, roughnessMap].forEach((t) => {
             t.wrapS = THREE.RepeatWrapping;
             t.wrapT = THREE.RepeatWrapping;
-            t.repeat.set(6, 6); 
+            t.repeat.set(6, 6);
         });
 
         normalMap.encoding = THREE.LinearEncoding;
@@ -93,23 +117,21 @@ function createPlane() {
     plane.rotation.set(Math.PI * -0.5, 0, 0);
 
     const plane2 = plane.clone()
-    plane2.position.set(0, 0, plane.geometry.parameters.height/2)
+    plane2.position.set(0, 0, plane.geometry.parameters.height / 2)
 
     const group = new THREE.Group()
     group.add(plane, plane2)
-
     return group
 }
 const planes = createPlane()
 scene.add(planes)
 
 function movePlane() {
-    planes.children.forEach((plane)=>{
+    planes.children.forEach((plane) => {
         plane.position.z += 0.01;
-        if(plane.position.z >= 25) plane.position.z = -25
+        if (plane.position.z >= 25) plane.position.z = -25
     })
 }
-movePlane();
 
 //post processing
 const composer = new EffectComposer(renderer);
@@ -121,6 +143,7 @@ const player = new Player(scene, cssRenderer, enemy.list);
 
 async function loadPlayerEnemy() {
     await enemy.initialize();
+    updateProgress(50)
     await player.initialize();
 
     const bloomEffect = new SelectiveBloomEffect(scene, camera, {
@@ -133,19 +156,18 @@ async function loadPlayerEnemy() {
     composer.addPass(new EffectPass(camera, new SMAAEffect({
         preset: 3
     })))
+    updateProgress(50)
 }
 
-await loadPlayerEnemy() //load before executing code below
+loadPlayerEnemy() //load before executing code below
 
 //resize
 window.addEventListener('resize', () => {
-    width = window.innerWidth;
-    height = window.innerHeight;
-    composer.setSize(width, height)
-    cssRenderer.setSize(width, height);
+    composer.setSize(innerWidth, innerHeight)
+    cssRenderer.setSize(innerWidth, innerHeight);
 
     //resize camera
-    camera.aspect = width / height;
+    camera.aspect = innerWidth / innerHeight;
     camera.updateProjectionMatrix();
 
     //player.resize();
